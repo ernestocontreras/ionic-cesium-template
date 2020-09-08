ff968e6c87bfa33c49c3e20d51d372cc6f0b46dc
Ernesto Contreras <ernesto.contreras@decsef.com>
2020-09-08 12:14:07 -0500

    Geolocation integration

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,6 +6,7 @@ import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @NgModule({
   imports: [
@@ -14,6 +15,7 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  providers: [Geolocation]
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Geolocation, Geoposition } from '@ionic-native/geolocation/ngx';
+import { Platform } from '@ionic/angular';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -9,21 +12,41 @@ export class HomePage {
 
   @ViewChild('cesiumViewer', null) cesiumViewer: ElementRef<HTMLDivElement>;
 
+  /**
+   * The main {Cesium.Viewer}
+   */
   private viewer;
 
-  constructor() {}
+  coords = {
+    altitude: 0,
+    latitude: 0,
+    longitude: 0
+  };
+
+  follow = true;
+
+  constructor(private geolocation: Geolocation, private platform: Platform) { }
 
   ngAfterViewInit(): void {
     this.viewer = this.createViewer(this.cesiumViewer.nativeElement);
+    this.viewer.entities.add(this.createUserPositionIcon('ID_USER'));
+
+    // Watch position changes
+    this.platform.ready().then(() =>
+      this.geolocation
+        .watchPosition({ enableHighAccuracy: true, maximumAge: 0 })
+        .pipe(filter(x => (x as any).coords)) // Only if it has coordinates 
+        .subscribe(pos => this.handleNewPosition((pos as Geoposition).coords))
+    );
   }
 
   /**
    * Creates and returns a new cesium viewer instance.
    * 
-   * @param {HTMLDivElement} element 
-   * @return {Cesium.Viewer} the new instance
+   * @param element 
+   * @return {Cesium.Viewer} The new viewer instance
    */
-  private createViewer(element: HTMLDivElement | string): any {
+  private createViewer(element: HTMLDivElement): any {
     Cesium.Label.enableRightToLeftDetection = true;
     return new Cesium.Viewer(element, {
       fullscreenButton: false,
@@ -36,14 +59,65 @@ export class HomePage {
       navigationHelpButton: false,
       navigationInstructionsInitiallyVisible: false,
       imageryProvider: null,
-      baseLayerPicker: false,
+      baseLayerPicker: true,
       animation: false,
-      terrainProvider: null,
       selectionIndicator: false,
       projectionPicker: false,
       sceneMode: Cesium.SceneMode.SCENE2D,
       shadows: false,
       orderIndependentTranslucency: false
+    });
+  }
+
+  /**
+   * Creates and returns a new cesium entity with the given id.
+   * 
+   * @param id
+   * @return {Cesium.Entity} The new entity instance
+   */
+  private createUserPositionIcon(id: string): any {
+    return new Cesium.Entity({
+      id: id,
+      name: 'User Position',
+      editable: false,
+      own: true,
+      cc: 0,
+      position: Cesium.Cartesian3.fromDegrees(0, 0, 0),
+      billboard: {
+        image: "assets/maki/marker.png",
+        scale: 2.0,
+        color: Cesium.Color.LAWNGREEN,
+        width: 25,
+        height: 25
+      },
+    });
+  }
+
+  /**
+   * Handle when exists a new position.
+   * 
+   * @param coords The new position coordinates.
+   */
+  private handleNewPosition(coords: Coordinates): void {
+    // Update entity position
+    const entity = this.viewer.entities.getById('ID_USER');
+    entity.position = Cesium.Cartesian3.fromDegrees(coords.longitude, coords.latitude, coords.altitude);
+
+    if (this.follow) {
+      this.centerPosition();
+    }
+
+    // Update inticators
+    this.coords = coords;
+  }
+
+  /**
+   * Center Cesium camera to the user position
+   */
+  centerPosition(): void {
+    this.viewer.scene.camera.flyTo({
+      destination: Cesium.Cartesian3.fromDegrees(this.coords.longitude, this.coords.latitude, 3000),
+      duration: 0
     });
   }
 }
